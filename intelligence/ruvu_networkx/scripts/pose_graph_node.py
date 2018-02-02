fff80b41774a42d61645033904003af9aa8d368c
Ramon Wijnands <ramon@ruvu.nl>
2018-02-02 14:56:40 +0100

    Fix all parameters should be private

--- intelligence/ruvu_networkx/scripts/pose_graph_node.py
+++ intelligence/ruvu_networkx/scripts/pose_graph_node.py
@@ -366,10 +366,10 @@ class PoseGraphNode(object):
 if __name__ == "__main__":
     rospy.init_node("pose_graph_node")
     pgn = PoseGraphNode(
-        rospy.get_param("frame_id", "map"),
-        rospy.get_param("robot_frame_id", "base_link"),
-        rospy.get_param("file_path", "/tmp/pose_graph.yaml"),
-        rospy.get_param("click_timeout", 5.0),
-        rospy.get_param("interpolation_distance", 0.2)
+        rospy.get_param("~frame_id", "map"),
+        rospy.get_param("~robot_frame_id", "base_link"),
+        rospy.get_param("~file_path", "/tmp/pose_graph.yaml"),
+        rospy.get_param("~click_timeout", 5.0),
+        rospy.get_param("~interpolation_distance", 0.2)
     )
     rospy.spin()
