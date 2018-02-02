991cac19723d12fcbd5fd83440f2a5cf0f9babe6
Ramon Wijnands <ramon@ruvu.nl>
2018-02-02 12:56:47 +0100

    Add a test for ros_visualization.py

---
+++ intelligence/ruvu_networkx/test/test_ros_visualization.py
@@ -0,0 +1,10 @@
+import networkx
+from mock import patch
+from ruvu_networkx.ros_visualization import get_visualization_marker_array_msg_from_pose_graph
+
+@patch('rospy.Time.now')
+def test_empty_graph(now):
+    now.return_value = 3
+    G = networkx.Graph()
+    m = get_visualization_marker_array_msg_from_pose_graph(G, 'frame_id')
+    assert m
