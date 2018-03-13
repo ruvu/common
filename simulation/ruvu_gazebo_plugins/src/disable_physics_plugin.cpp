51d82bf649cd0b060a8bc1fe86e531fe7a972924
Rein Appeldoorn <rein@ruvu.nl>
2018-03-13 13:42:01 +0100

    chore: fixed linting ruvu_gazebo_plugins

--- simulation/ruvu_gazebo_plugins/src/disable_physics_plugin.cpp
+++ simulation/ruvu_gazebo_plugins/src/disable_physics_plugin.cpp
@@ -1,17 +1,20 @@
+//
+// Copyright (c) 2017 RUVU Robotics
+//
+// @author Rein Appeldoorn
+//
+
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 
 namespace gazebo
 {
-
 //!
 //! \brief The DisablePhysicsPlugin class disables the physics engine when it is on
 //!
 class DisablePhysicsPlugin : public WorldPlugin
 {
-
 public:
-
   //!
   //! \brief Load Register update function
   //! \param world Pointer to the world
@@ -19,12 +22,10 @@ public:
   void Load(physics::WorldPtr world, sdf::ElementPtr /*_sdf*/)
   {
     world_ = world;
-    update_connection_ = event::Events::ConnectWorldUpdateBegin(
-           boost::bind(&DisablePhysicsPlugin::OnUpdate, this, _1));
+    update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&DisablePhysicsPlugin::OnUpdate, this, _1));
   }
 
 protected:
-
   //!
   //! \brief _world Pointer to physics world instance
   //!
@@ -43,10 +44,8 @@ protected:
     if (world_->GetEnablePhysicsEngine())
       world_->EnablePhysicsEngine(false);
   }
-
 };
 
 // Register this plugin with the simulator
 GZ_REGISTER_WORLD_PLUGIN(DisablePhysicsPlugin)
-
-}
+}  // namespace gazebo
