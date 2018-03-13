814d6c15116f32e52b30c2e8bf489d56b7783131
Rein Appeldoorn <rein@ruvu.nl>
2018-03-13 13:47:20 +0100

    chore: fixed linting ruvu_networkx

--- intelligence/ruvu_networkx/test/test_ros_visualization.py
+++ intelligence/ruvu_networkx/test/test_ros_visualization.py
@@ -2,6 +2,7 @@ import networkx
 from mock import patch
 from ruvu_networkx.ros_visualization import get_visualization_marker_array_msg_from_pose_graph
 
+
 @patch('rospy.Time.now')
 def test_empty_graph(now):
     now.return_value = 3
