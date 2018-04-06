2226f2b3be5d9c662042ce2e8da0f22ec04c570a
Rein Appeldoorn <rein@ruvu.nl>
2018-04-06 13:55:55 +0200

    feat(scripts/connect_to_external_ros_master.bash) Streamline setting up external ros master connection

---
+++ bringup/ruvu_bringup/scripts/connect_to_external_ros_master.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+_error() {
+    echo -e "\033[31m$1\033[0m"
+}
+
+if [ -z "$1" ]
+then
+    _error "Please provide a [ros_master_uri, e.g. http://192.168.1.2:11311] as argument"
+else
+    if [ -z "$2" ]
+    then
+        _error "Please provide a [network_interface, e.g. eno1, eth0] as argument"
+    else
+       source `rospack find ruvu_bringup`/scripts/set_ros_master_uri.bash $1
+       source `rospack find ruvu_bringup`/scripts/set_ros_ip_from_network_interface.bash $2
+    fi
+fi
+
+unset _error
+
