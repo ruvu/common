64ad7651947e3d92a3690687adc6b7e8c5bbe161
Rein Appeldoorn <rein@ruvu.nl>
2018-06-06 14:23:16 +0200

    feat: cancel all goals

---
+++ desktop/ruvu_rviz_plugins/src/cancel_all_goals_tool.cpp
@@ -0,0 +1,47 @@
+//
+// Copyright (c) 2018 RUVU Robotics
+//
+// @author Rein Appeldoorn
+//
+
+#include "./cancel_all_goals_tool.h"
+
+#include <actionlib_msgs/GoalID.h>
+#include <rviz/properties/string_property.h>
+#include <rviz/properties/float_property.h>
+#include <string>
+
+namespace ruvu_rviz_plugins
+{
+CancelAllGoalsTool::CancelAllGoalsTool()
+  : topic_property_(new rviz::StringProperty("Topic", "goal", "The topic on which to publish the joint position goal.",
+                                             getPropertyContainer(), SLOT(updateTopic()), this))
+{
+}
+
+void CancelAllGoalsTool::onInitialize()
+{
+  updateTopic();
+}
+
+void CancelAllGoalsTool::updateTopic()
+{
+  pub_ = nh_.advertise<actionlib_msgs::GoalID>(topic_property_->getStdString(), 1);
+}
+
+void CancelAllGoalsTool::activate()
+{
+  pub_.publish(actionlib_msgs::GoalID());
+  Q_EMIT(close());
+}
+void CancelAllGoalsTool::deactivate()
+{
+}
+
+int CancelAllGoalsTool::processMouseEvent()
+{
+}
+}  // namespace ruvu_rviz_plugins
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(ruvu_rviz_plugins::CancelAllGoalsTool, rviz::Tool)
---
+++ desktop/ruvu_rviz_plugins/src/cancel_all_goals_tool.h
@@ -0,0 +1,45 @@
+//
+// Copyright (c) 2018 RUVU Robotics
+//
+// @author Rein Appeldoorn
+//
+
+#pragma once
+
+#include <ros/node_handle.h>
+#include <ros/publisher.h>
+#include <rviz/tool.h>
+#include <string>
+
+// forward declare
+namespace rviz
+{
+class StringProperty;
+class FloatProperty;
+}
+
+namespace ruvu_rviz_plugins
+{
+class CancelAllGoalsTool : public rviz::Tool
+{
+  Q_OBJECT
+
+public:
+  CancelAllGoalsTool();
+  virtual void onInitialize();
+
+  virtual void activate();
+  virtual void deactivate();
+
+  virtual int processMouseEvent();
+
+private Q_SLOTS:
+  void updateTopic();
+
+private:
+  ros::NodeHandle nh_;
+  ros::Publisher pub_;
+
+  rviz::StringProperty* topic_property_;
+};
+}  // namespace ruvu_rviz_plugins
