ba39e1ece7813d9585c59ebfa0316cfeff4469ff
Rein Appeldoorn <rein@ruvu.nl>
2018-07-04 17:29:44 +0200

    correct input args

--- hardware/pozyx_ros/src/pozyx_ros/two_tag_positioner/algorithms/multi_tag_positioner.py
+++ hardware/pozyx_ros/src/pozyx_ros/two_tag_positioner/algorithms/multi_tag_positioner.py
@@ -2,5 +2,5 @@ class MultiTagPositioner(object):
     def __init__(self, anchor_locations, tag_locations):
         pass
 
-    def get_positions(self):
+    def get_positions(self, input, ranges):
         return []
