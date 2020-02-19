beb546a14ba186496cdbbc9e1b98f5761c59f293
Paul Verhoeckx <paul@ruvu.nl>
2020-02-19 08:45:34 +0100

    Add PoseStamped <-> TransformStamped conversion functions

---
+++ utils/ruvu_msg_converters/src/ruvu_msg_converters/__init__.py
@@ -0,0 +1,2 @@
+from .pose_stamped_to_transform_stamped import pose_stamped_to_transform_stamped
+from .transform_stamped_to_pose_stamped import transform_stamped_to_pose_stamped
---
+++ utils/ruvu_msg_converters/src/ruvu_msg_converters/pose_stamped_to_transform_stamped.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from geometry_msgs.msg import PoseStamped, TransformStamped
+
+
+def pose_stamped_to_transform_stamped(msg, child_frame_id=""):
+    """
+    Convert a TransformStamped to a PoseStamped message
+    :param msg: geometry_msgs/PoseStamped
+    :param child_frame_id: String
+    :return: geometry_msgs/TransformStamped
+    """
+    assert isinstance(msg, PoseStamped)
+    ts = TransformStamped()
+    ts.header = msg.header
+    ts.child_frame_id = child_frame_id
+    ts.transform.translation.x = msg.pose.position.x
+    ts.transform.translation.y = msg.pose.position.y
+    ts.transform.translation.z = msg.pose.position.z
+    ts.transform.rotation = msg.pose.orientation
+    return ts
