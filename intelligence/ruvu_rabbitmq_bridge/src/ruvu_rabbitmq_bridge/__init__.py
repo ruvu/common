ca0d9324b1d9115c93cf5bd09aa2174cc6fbbda9
Paul Verhoeckx <paul@ruvu.nl>
2020-06-11 14:53:57 +0200

    First implementation of coupling between ROS and RPC servers and clients

--- intelligence/ruvu_rabbitmq_bridge/src/ruvu_rabbitmq_bridge/__init__.py
+++ intelligence/ruvu_rabbitmq_bridge/src/ruvu_rabbitmq_bridge/__init__.py
@@ -1,4 +1,4 @@
-from roslib.message import get_message_class
+from roslib.message import get_message_class, get_service_class
 from collections import namedtuple
 
 TopicBridge = namedtuple("TopicBridge", "topic message_type durable exchange")
@@ -18,6 +18,27 @@ def get_topic_bridges(param_value):
             raise ValueError("Bridge item should be a dictionary")
         try:
             item['message_type'] = get_message_class(item['message_type'])
+            bridges.append(TopicBridge(**item))
+        except KeyError as e:
+            raise ValueError('Missing key {} in topic bridge item'.format(e))
+
+    return bridges
+
+
+def get_service_bridges(param_value):
+    """
+    :param param_value: Parameter dictionary value
+    :return: A list with named tuples
+    """
+    if not isinstance(param_value, list):
+        raise ValueError("Should be a list")
+
+    bridges = []
+    for item in param_value:
+        if not isinstance(item, dict):
+            raise ValueError("Bridge item should be a dictionary")
+        try:
+            item['message_type'] = get_service_class(item['message_type'])
             bridges.append(TopicBridge(**item))
         except KeyError as e:
             raise ValueError('Missing key {} in topic bridge item'.format(e))
