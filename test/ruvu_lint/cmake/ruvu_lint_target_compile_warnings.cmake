d2257402250edb0a2143dcbdfe9ddfba4640b67f
Ramon Wijnands <ramon@ruvu.nl>
2020-09-11 16:43:06 +0200

    Add default compile warnings to ruvu_lint

---
+++ test/ruvu_lint/cmake/ruvu_lint_target_compile_warnings.cmake
@@ -0,0 +1,38 @@
+# Inspired by https://github.com/lefticus/cpp_starter_project/blob/master/cmake/CompilerWarnings.cmake
+
+function(target_compile_warnings target)
+  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
+
+  set(GENERIC_WARNINGS
+      -Werror
+      -Wall
+      -Wextra
+      -Wno-unused-parameter # several ROS libraries have unused parameters
+      -Wcast-align
+      -Woverloaded-virtual
+      -Wformat=2
+      # some extra warnings I found on stackoverflow
+      -Wdisabled-optimization
+      -Wredundant-decls
+      -Wsign-promo
+      -Wstrict-overflow=5
+      -Wswitch-default
+      ${ARGN}
+  )
+
+  set(GCC_WARNINGS
+      ${GENERIC_WARNINGS}
+      -Wnoexcept
+      -Wstrict-null-sentinel
+  )
+
+  if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    set(PROJECT_WARNINGS ${GENERIC_WARNINGS})
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(PROJECT_WARNINGS ${GCC_WARNINGS})
+  else()
+    message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
+  endif()
+
+  target_compile_options(${target} PRIVATE ${PROJECT_WARNINGS})
+endfunction()
