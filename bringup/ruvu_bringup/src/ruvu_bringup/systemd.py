511bec9dd784eadc8f14911a9a663c495ebb43ff
Ramon Wijnands <ramon@ruvu.nl>
2020-10-08 14:14:47 +0200

    Fix systemd environment to run launch files
    If we include ~/.local/bin we can use ros-get from systemd. This will
    allow us to source ~/.ruvu/setup.bash which can automatically determine
    to source from devel or install space.

--- bringup/ruvu_bringup/src/ruvu_bringup/systemd.py
+++ bringup/ruvu_bringup/src/ruvu_bringup/systemd.py
@@ -9,6 +9,7 @@ WantedBy=multi-user.target
 [Service]
 Restart=on-abort
 User=%s
+Environment=PATH=~/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 ExecStart=/bin/bash -c "source %s && roslaunch %s %s --wait"
 """
 
